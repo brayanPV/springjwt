e6b39cb2998ec96b7728c8b237cfd1d5242f544c
brayanPV <brayanstivenpv@ufps.edu.co>
2020-12-31 15:06:06 -0500

    login con formato json

--- src/main/java/datajpacom/example/proyectoconjpa/auth/filter/JWTAuthenticationFilter.java
+++ src/main/java/datajpacom/example/proyectoconjpa/auth/filter/JWTAuthenticationFilter.java
@@ -1,6 +1,9 @@
 package datajpacom.example.proyectoconjpa.auth.filter;
 
 import java.io.IOException;
+import java.util.Base64;
+import java.util.Collection;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -10,16 +13,22 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+import org.hibernate.annotations.common.util.impl.Log_.logger;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import datajpacom.example.proyectoconjpa.entities.Usuario;
+import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
@@ -36,17 +45,25 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
         String username = obtainUsername(request);
         String password = obtainPassword(request);
 
-        if (username == null) {
-            username = "";
-        }
-
-        if (password == null) {
-            password = "";
-        }
 
         if (username != null && password != null) {
             logger.info("Username desde form-data en postman: " + username);
             logger.info("Password desde form-data en postman: " + password);
+        } else {
+            try {
+                Usuario user = new ObjectMapper().readValue(request.getInputStream(), Usuario.class);
+                username = user.getUsername();
+                password = user.getPassword();
+                logger.info("Username desde RAW en postman: " + username);
+                logger.info("Password desde RAW en postman: " + password);
+            } catch (JsonParseException e) {
+                e.printStackTrace();
+            } catch (JsonMappingException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
         }
         username = username.trim();
 
@@ -65,10 +82,18 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
             Authentication authResult) throws IOException, ServletException {
         // TODO Auto-generated method stub
         String username = ((User) authResult.getPrincipal()).getUsername();
+        Collection<? extends GrantedAuthority> roles = authResult.getAuthorities();
+        Claims claims = Jwts.claims();
+        claims.put("authorities", new ObjectMapper().writeValueAsString(roles));
         SecretKey secretKey = Keys.secretKeyFor(SignatureAlgorithm.HS512);
+        //imprimir la key
+        //String encodedKey = Base64.getEncoder().encodeToString(secretKey.getEncoded());
+        //logger.info("KEY: " + encodedKey);
         String token = Jwts.builder()
         .setSubject(username)
         .signWith(secretKey)
+        .setIssuedAt(new Date())
+        .setExpiration(new Date(System.currentTimeMillis() + 3600000 * 4)) //3600000 = 1 hora 
         .compact();
 
         response.addHeader("Authorization", "Bearer " + token);
